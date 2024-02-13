117edf3aa482e5c41c61e126201ee19eb8d048e4
tymatall123git config --global user.name Your <fatoumatatall101@gmail.com>
2024-02-13 16:19:58 +0000

    retouche

--- src/app/interface-User/acceuil/acceuil.component.ts
+++ src/app/interface-User/acceuil/acceuil.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// import * as bootstrap from 'bootstrap'; 
+// import * as popper from 'popper.js';    
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
